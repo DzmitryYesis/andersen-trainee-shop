07648fc7f9aadc6c608c3f0a88e15062309857ad
DzmitryYesis <yesis1988@mail.ru>
2022-08-22 19:55:26 +0300

    feat: get data from server and render product

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import style from './App.module.css';
 import { Authorization } from './components/authorization/Authorization';
 import { Header } from './components/header/Header';
+import { MainPage } from './components/mainPage/MainPage';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -13,6 +14,7 @@ const App = () => {
       <header className={style.header}>
         <Header setIsShowPopUp={setIsShowPopUp} />
       </header>
+      <MainPage />
       <Authorization isShowPopUp={isShowPopUp} setIsShowPopUp={setIsShowPopUp} />
     </div>
   );
---
+++ src/components/mainPage/MainPage.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import style from './MainPage.module.css';
+
+export const MainPage = () => {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => setState(data));
+  }, []);
+
+  return (
+    <div className={style.asd}>
+      {state?.map(product => (
+        <div className={style.productWrapper} key={product.id}>
+          <img
+            className={style.productImg}
+            src={product.images[0]}
+            alt="Images of product"
+          />
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+          <button type="button">Add</button>
+        </div>
+      ))}
+    </div>
+  );
+};
