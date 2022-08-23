83da13865d4e69b918c5eda6024d6fd44b5fbc54
DzmitryYesis <yesis1988@mail.ru>
2022-08-23 13:57:01 +0300

    feat: create ItemsPage component

---
+++ src/components/itemsPage/ItemsPage.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import style from './ItemsPage.module.css';
+
+export const ItemsPage = ({ isLoggedIn }) => {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => setState(data));
+  }, []);
+
+  return (
+    <div className={style.asd}>
+      {state?.map(product => (
+        <div className={style.productWrapper} key={product.id}>
+          <img
+            className={style.productImg}
+            src={product.images[0]}
+            alt="Images of product"
+          />
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+          <button type="button">{isLoggedIn ? 'Add' : 'Login for added'}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
