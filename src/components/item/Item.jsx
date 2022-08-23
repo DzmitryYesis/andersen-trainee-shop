228c20f7124db90e9a9f4c9d9098895fc228eba0
DzmitryYesis <yesis1988@mail.ru>
2022-08-23 16:51:40 +0300

    refactor: add some style for Item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import style from './Item.module.css';
+
 export const Item = () => {
   const { id } = useParams();
   const [item, setItem] = useState(null);
@@ -14,12 +16,18 @@ export const Item = () => {
 
   return (
     <div>
-      <span>{item?.title}</span>
-      <span>{item?.price}</span>
-      <span>{item?.description}</span>
-      <img src={item?.images[0]} alt="item-view" />
-      <img src={item?.images[1]} alt="item-view" />
-      <img src={item?.images[2]} alt="item-view" />
+      <div className={style.info}>
+        <span>{item?.title}</span>
+        <span>{item?.price}</span>
+        <span>{item?.description}</span>
+      </div>
+      <div className={style.wrapperImg}>
+        <img className={style.mainImg} src={item?.images[0]} alt="item-view" />
+        <div className={style.secondRowImg}>
+          <img className={style.otherImg} src={item?.images[1]} alt="item-view" />
+          <img className={style.otherImg} src={item?.images[2]} alt="item-view" />
+        </div>
+      </div>
     </div>
   );
 };
