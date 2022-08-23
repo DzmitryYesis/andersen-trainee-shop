3d0647b75cd51afb4d98576a464b64ab3c6216a1
DzmitryYesis <yesis1988@mail.ru>
2022-08-23 13:57:29 +0300

    feat: add react-router-dom and create routes

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import style from './MainPage.module.css';
+import { ItemsPage } from '../itemsPage/ItemsPage';
+import { ShopPage } from '../shopPage/ShopPage';
 
-export const MainPage = ({ isLoggedIn }) => {
-  const [state, setState] = useState(null);
-
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(res => res.json())
-      .then(data => setState(data));
-  }, []);
-
-  return (
-    <div className={style.asd}>
-      {state?.map(product => (
-        <div className={style.productWrapper} key={product.id}>
-          <img
-            className={style.productImg}
-            src={product.images[0]}
-            alt="Images of product"
-          />
-          <p>{product.title}</p>
-          <p>{product.price}</p>
-          <button type="button">{isLoggedIn ? 'Add' : 'Login for added'}</button>
-        </div>
-      ))}
-    </div>
-  );
-};
+export const MainPage = ({ isLoggedIn }) => (
+  <div>
+    <Routes>
+      <Route path="/" component={<ItemsPage isLoggedIn={isLoggedIn} />} exact />
+      <Route path="/shop-information" component={<ShopPage />} />
+    </Routes>
+  </div>
+);
