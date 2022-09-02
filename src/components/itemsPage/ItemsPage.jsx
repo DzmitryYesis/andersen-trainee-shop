8d7b2ae7b2006b2f27bc99175a390b713dc564e6
DzmitryYesis <yesis1988@mail.ru>
2022-09-02 08:48:15 +0300

    refactor: formatting code fix bugs

--- src/components/itemsPage/ItemsPage.jsx
+++ src/components/itemsPage/ItemsPage.jsx
@@ -1,17 +1,32 @@
 import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { selectItems } from '../../store/selectors/itemsSelectors';
 import { getUsersTC } from '../../store/thunks/itemsThunks';
 
 import { Item } from './item/Item';
+import style from './ItemsPage.module.css';
 
 export const ItemsPage = () => {
   const dispatch = useDispatch();
+  const state = useSelector(selectItems);
 
   useEffect(() => {
     dispatch(getUsersTC());
   }, []);
 
-  return <Item />;
+  return (
+    <div className={style.wrapper}>
+      {state?.map(product => (
+        <Item
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          image={product.image}
+          price={product.price}
+        />
+      ))}
+    </div>
+  );
 };
--- src/components/itemsPage/item/Item.jsx
+++ src/components/itemsPage/item/Item.jsx
@@ -2,59 +2,46 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { addItemInCartAC } from '../../../store/actions/cartActions';
-import { selectItems } from '../../../store/selectors/itemsSelectors';
 import { selectIsLoggedIn } from '../../../store/selectors/loginSelectors';
 
 import style from './Item.module.css';
 
-export const Item = () => {
+export const Item = ({ id, image, title, price }) => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const state = useSelector(selectItems);
 
-  const handleAddItem = product => {
+  const handleAddItem = () => {
     const item = {
-      image: product.image,
-      id: product.id,
-      title: product.title,
-      price: product.price,
+      image,
+      id,
+      title,
+      price,
       totalCount: 1,
-      totalPrice: product.price,
+      totalPrice: price,
     };
     dispatch(addItemInCartAC(item, 1));
   };
+
   return (
-    <div className={style.wrapper}>
-      {state?.map(product => (
-        <div className={style.productWrapper} key={product.id}>
-          <div className={style.divImg}>
-            <img
-              className={style.productImg}
-              src={product.image}
-              alt="Images of product"
-            />
-          </div>
-          <div className={style.divTitle}>
-            <Link key={product.id} to={`/item/${product.id}`}>
-              {product.title}
-            </Link>
-          </div>
-          <p>Price: {product.price}</p>
-          {isLoggedIn ? (
-            <button
-              className={style.button}
-              type="button"
-              onClick={() => handleAddItem(product)}
-            >
-              Add
-            </button>
-          ) : (
-            <button className={style.button} type="button">
-              Login for added
-            </button>
-          )}
-        </div>
-      ))}
+    <div className={style.productWrapper} key={id}>
+      <div className={style.divImg}>
+        <img className={style.productImg} src={image} alt="Images of product" />
+      </div>
+      <div className={style.divTitle}>
+        <Link key={id} to={`/item/${id}`}>
+          {title}
+        </Link>
+      </div>
+      <p>Price: {price}</p>
+      {isLoggedIn ? (
+        <button className={style.button} type="button" onClick={handleAddItem}>
+          Add
+        </button>
+      ) : (
+        <button className={style.button} type="button">
+          Login for added
+        </button>
+      )}
     </div>
   );
 };
